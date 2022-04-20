20ce5a1d032b9d6515728f024a7ff7c8fd7d74a2
TAaHwa <100752582+TAaHwa@users.noreply.github.com>
2022-04-20 14:30:55 +0900

    Add files via upload

---
+++ wk07/style.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!-- 이 부분은 주석문입니다. 웹 브라우저는 주석을 화면에 출력하지 않습니다. -->
+<html>
+	<head>
+		<!-- 문서제목, 자바스크립트 코드, CSS 스타일 정의, 메타데이터정의 -->
+		<meta charset="utf-8">
+		<title>hm08 : iframe - target control</title>
+		<style>
+		</style>
+		<script></script>
+	</head>
+
+	<body>
+		<!-- 문서의 본문 텍스트, 이미지, 테이블, 자바스크립트 코드, 동영상 등 -->
+		<h4>hm08 Info. Menu</h4>
+		
+        <!-- <nav id="menubar">
+            <ul>
+                <li>
+                    <a href="https://ko.wikipedia.org/wiki/%ED%81%AC%EB%A6%AC%EC%8A%A4%EB%A7%88%EC%8A%A4" target="right">ChristMas</a>
+                </li>
+                <li>
+                    <a href="https://youtu.be/DFH2NpzgQ2E" target="_blank">Carols</a>
+                </li>
+                <li>
+                    <a href="https://namu.wiki/w/%EC%82%B0%ED%83%80%ED%81%B4%EB%A1%9C%EC%8A%A4" target="right">Santa</a>
+                </li>
+                <li>
+                    <a href="https://dinorunner.com/ko/santa/" target="left">Santa Game</a>
+                
+                <li>
+                    <a href="hmxx_card.html" target="left">Main</a>
+                </li>
+            </ul> -->
+	</body>
+
+</html>
